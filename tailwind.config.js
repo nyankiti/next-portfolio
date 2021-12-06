53fb04d8f1e52350788e5c6def5812c4dbcbb1fb
nyankiti <nyancic77@outlook.jp>
2021-12-06 11:13:10 +0900

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx', './domain/**/*.tsx'],
   darkMode: 'class', // or 'media' or null
   theme: {
     fontFamily: {
@@ -9,6 +9,13 @@ module.exports = {
     boxShadow: {
       "custom-light": " 0 0 10px #313131",
       "custom-dark": "5px 5px 10px #0a0c0e , -5px -5px 10px #14161c",
+      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+      DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+      md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+      none: 'none',
     },
     extend: {
       colors: {
